b3d02b6bf1a2aa6d39d045d8a82e4760dcb771b3
pgyl <pgillet65@gmail.com>
2018-11-14 10:30:10 +0100

    Corrections mineures

--- pekislib/src/main/java/com/example/pgyl/pekislib_a/ClockAppAlarmUtils.java
+++ pekislib/src/main/java/com/example/pgyl/pekislib_a/ClockAppAlarmUtils.java
@@ -3,28 +3,27 @@ package com.example.pgyl.pekislib_a;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.AlarmClock;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
 import static com.example.pgyl.pekislib_a.MiscUtils.toastLong;
-import static com.example.pgyl.pekislib_a.TimeDateUtils.formattedCalendarTimeDate;
 import static com.example.pgyl.pekislib_a.TimeDateUtils.HHmm;
+import static com.example.pgyl.pekislib_a.TimeDateUtils.formattedCalendarTimeDate;
 
 public class ClockAppAlarmUtils {
 
     public static boolean setClockAppAlarm(Context context, long timeExp, String message) {
         boolean ret = false;
         Calendar calendar = Calendar.getInstance();    // Calendar => OK Time Zone
-        String timeNowAsHHMM = formattedCalendarTimeDate(calendar, HHmm);
+        String sTimeNow = formattedCalendarTimeDate(calendar, HHmm);
         calendar.setTimeInMillis(timeExp);
-        String timeExpAsHHMM = formattedCalendarTimeDate(calendar, HHmm);
+        String sTimeExp = formattedCalendarTimeDate(calendar, HHmm);
         int hourExp = calendar.get(Calendar.HOUR_OF_DAY);
         int minExp = calendar.get(Calendar.MINUTE);
         calendar.clear();
         calendar = null;
-        String errorMsg = "Setting Clock App alarm on " + timeExpAsHHMM;
-        if (!timeExpAsHHMM.equals(timeNowAsHHMM)) {
+        String settingMessage = "Setting Clock App alarm on " + sTimeExp;
+        if (!sTimeExp.equals(sTimeNow)) {
             Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
             intent.putExtra(AlarmClock.EXTRA_HOUR, hourExp);
             intent.putExtra(AlarmClock.EXTRA_MINUTES, minExp);
@@ -32,31 +31,29 @@ public class ClockAppAlarmUtils {
             intent.putExtra(AlarmClock.EXTRA_SKIP_UI, true);    //  Ne pas afficher Clock App
             if (intent.resolveActivity(context.getPackageManager()) != null) {
                 ret = true;
-                toastLong(errorMsg, context);
+                toastLong(settingMessage, context);
                 context.startActivity(intent);
             }
         }
         if (!ret) {
-            errorMsg = "Error " + errorMsg;
-            toastLong(errorMsg, context);
+            toastLong("Error " + settingMessage, context);
         }
         return ret;
     }
 
     public static boolean dismissClockAppAlarm(Context context, String message) {
         boolean ret = false;
-        String errorMsg = "Dismissing Clock App alarm " + message;
         Intent intent = new Intent(AlarmClock.ACTION_DISMISS_ALARM);
         intent.putExtra(AlarmClock.EXTRA_ALARM_SEARCH_MODE, AlarmClock.ALARM_SEARCH_MODE_LABEL);
         intent.putExtra(AlarmClock.EXTRA_MESSAGE, message);      //  On ne peut pas empêcher Clock App de s'afficher
+        String dismissingMessage = "Dismissing Clock App alarm " + message;
         if (intent.resolveActivity(context.getPackageManager()) != null) {
             ret = true;
-            Toast.makeText(context, errorMsg, Toast.LENGTH_LONG).show();
+            toastLong(dismissingMessage, context);
             context.startActivity(intent);
         }
         if (!ret) {
-            errorMsg = "Error " + errorMsg;
-            Toast.makeText(context, errorMsg, Toast.LENGTH_LONG).show();
+            toastLong("Error " + dismissingMessage, context);
         }
         return ret;
     }
